d0b4f2dbf0e73ff417211a7e19362f2711536716
ynfleozin <leonardoalvarengam@gmail.com>
2024-11-24 13:46:52 -0300

    add video with function

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ document.querySelectorAll('.faq-question').forEach((item) => {
         }
     });
 });
+
+function loadVideo(container) {
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('width', '100%');
+    iframe.setAttribute('height', '100%');
+    iframe.setAttribute('src', 'https://www.youtube.com/embed/pL9Og5eDaRc?autoplay=1');
+    iframe.setAttribute('title', 'YouTube video player');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('loading', 'lazy');
+    iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
+    iframe.setAttribute('referrerpolicy', 'strict-origin-when-cross-origin');
+    iframe.setAttribute('allowfullscreen', '');
+
+    container.innerHTML = '';
+    container.appendChild(iframe);
+}
+
